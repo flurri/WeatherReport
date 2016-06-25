ef684a7fd1449377306800a6d0e6de82af7bb7e3
Sara <snowflurrii@gmail.com>
2016-06-25 17:48:55 -0400

    Actual code!

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # WeatherReport
-Not actually involving the weather. Discord bot to automatically message RSS updates for webcomics.
+Not actually involving the weather. Relatively simple Discord bot to automatically message a channel RSS updates for webcomics.
+
+## Installation
+WeatherReport depends on [discord.js](https://www.npmjs.com/package/discord.js) and [libxmljs](https://www.npmjs.com/package/libxmljs).
+
+Make sure there is a `config.json` file in the same directory as `WeatherReport.js`. A template `config.json.sample` already exists in the directory.
+
+## Configuration
+`config.json.sample` contains all the necessary elements for configuration. More information on each is specified below.
+
+`token`: Discord bot token.
+`channel`: Discord server name, without the #. For example, if you want the bot to post to #general, use `"general"`.
+`feed_url`: RSS feed url.
+`polling_time`: Amount of time between RSS feed polls in seconds. `300` (5 minutes) is pretty safe.
+`header`: Text prepended to the update information. For example, the default of `"New comic: "` would show `"New Comic: Comic Title - Comic URL"`.
+`state_dir`: This holds the directory for state information if the bot is shut down.
+`debug`: Set to `true` to show extra debugging information.
+`sendMessage`: If `false`, the messages that would typically sent to the channel are instead sent to stdout.
---
+++ src/WeatherReport.js
@@ -0,0 +1,95 @@
+"use strict";
+
+var Discord = require("discord.js");
+var libxmljs = require("libxmljs");
+var http = require("http");
+var fs = require("fs");
+var config = require("./config.json");
+var state = null
+
+// called every time we poll for RSS updates (see main)
+function onRssPoll(botInstance, data) {
+    var doc = libxmljs.parseXml(data);
+    var title = doc.get("/rss/channel/item/title").text();
+    var link = doc.get("/rss/channel/item/link").text();
+    if (state !== link) {
+        if (config.debug) console.log("New link: %s", link);
+        if (config.sendMessage === false) {
+            console.log("Would have sent: %s%s - %s", config.header, title, link);
+            state = link;
+            return;
+        }
+        botInstance.sendMessage(botInstance.channels.get("name", config.channel),
+                                config.header + title + " - " + link, (err) => {
+                                    if (err) {
+                                        console.error(err);
+                                        cleanupAndQuit(botInstance, state, "Couldn't send message");
+                                    } else {
+                                        state = link;
+                                    }
+        });
+    } else if (config.debug) console.log("Nothing new...");
+}
+
+// quit cleanly-- save state info, properly logout, etc
+function cleanupAndQuit(botInstance, state, errorInfo) {
+    if (state != null) {
+        fs.writeFileSync(config.state_dir + "/state", state);
+    }
+    console.error("Quitting, reason: %s", errorInfo);
+    botInstance.destroy((err) => {
+        if (err) console.error(err);
+    });
+    process.exit();
+}
+
+function main() {
+    var bot = new Discord.Client();
+
+    // make sure we exit cleanly on sigint (^C)
+    process.on("SIGINT", () => {
+       cleanupAndQuit(bot, null, "Caught interrupt signal");
+    });
+
+    // get last known state (if any)
+    try {
+        if (fs.statSync(config.state_dir + "/state").isFile()) {
+            state = fs.readFileSync(config.state_dir + "/state");
+        }
+    } catch (e) {
+        if (config.debug) console.log(e);
+    }
+
+    bot.on("ready", function () {
+        // make sure the channel even exists before we bother with anything else
+        if (config.debug) console.log("Readying... checking channel...");
+        if (bot.channels.has("name", config.channel) === false) {
+            console.error("Channel #%s does not exist", config.channel);
+            cleanupAndQuit(bot, null, "Channel error");
+        }
+        if (config.debug) console.log("Ready!");
+        setInterval(() => {
+            if (config.debug) console.log("Getting %s", config.feed_url);
+            http.get(config.feed_url, (res) => {
+                var pageData = "";
+                res.setEncoding("utf8");
+                res.on("data", (rssData) => {
+                    pageData += rssData;
+                });
+                res.on("end", () => {
+                    onRssPoll(bot, pageData);
+                });
+                res.on("error", (err) => {
+                    console.error(err);
+                    cleanupAndQuit(bot, state, "HTTP Error");
+                });
+            });
+        }, config.polling_time*1000);
+    });
+
+    bot.loginWithToken(config.token);
+}
+
+if (require.main === module) {
+    main();
+}
